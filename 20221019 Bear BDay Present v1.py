69cf151eb8f8466aaa846f82821f15abb4bd8267
Terrance Oey <oey.terrance@gmail.com>
2022-10-20 07:09:09 -0400

    Initial commit of cyoa repo.

---
+++ 20221019 Bear BDay Present v1.py
@@ -0,0 +1,24 @@
+
+
+print('Ring, ring, ring. Ring, ring, ring.', '\n'
+, 'Fuck you thought. You glance at the clock on your nightstand.', '\n'
+, 'The faded green light on your clock flickers 2:34 a.m. Who the hell is calling your this early you thought.', '\n'
+, '\n'
+, '\"This better be important.\" you said in a groggy voice.', '\n'
+, '\"Is this Detective Bear?\" asked the faint voice.', '\n'
+, '\"I gave up that gig a long time ago. Contact Detective Sponholtz. He should be able to help you out.\" you replied about to hang up your cell phone.', '\n'
+, '\"He\'s dead.\"', '\n'
+, '\n'
+, 'Your mind and body jolt awake. Detective Sponholtz may not have been the best detective at the agency but he was far from incompetent. He was actually your senior but you rose through the ranks faster than him. People called you lucky but deep down they and you knew that it was far from luck.', '\n'
+, '\n'
+, '\"Come again?\" you ask.', '\n'
+, 'The voice continues, \"The police called just a few minutes ago. They didn\'t go into details but they said one word: Mutilation.\"', '\n'
+, '\"Mutilation? What were the circumstances surrounding this? How did it happen?\" you quickly asked.', '\n'
+, '\"Look, like I said, the police didn\'t go into details. I\'m calling you because I need you to take on Detective Sponholtz\'s case.\" she bluntly said.', '\n'
+, '\n'
+'You search your mind for a few moments and finally asked \"You mean the spooky mansion case?\"', '\n'
+'\"Yes.\" she said.', '\n'
+, '\n'
+, 'The spooky mansion.'
+
+, sep='')
